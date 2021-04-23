cbb1e7325ffc068daad3aee61fca18b853692582
jzohdi <31998568+jzohdi@users.noreply.github.com>
2021-04-23 10:14:37 -0400

    replacing styles with flake css use

--- examples/nextjs/nextjs-flake-example/pages/_document.js
+++ examples/nextjs/nextjs-flake-example/pages/_document.js
@@ -1,4 +1,18 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { css, extractCss, FLAKE_STYLE_ID } from "@sightread/flake"
+
+const classes = css({
+  root: {
+    padding: "0px",
+    margin: "0px",
+    fontFamily: `-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, 
+        Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif`,
+    '& a': {
+      color: "inherit",
+      textDecoration: "none",
+    },
+  }
+})
 
 /**  basic document from: 
  * https://nextjs.org/docs/advanced-features/custom-document
@@ -11,8 +25,10 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
-        <Head />
+      <Html className={classes.root}>
+        <Head>
+        <style id={FLAKE_STYLE_ID} dangerouslySetInnerHTML={{ __html: extractCss() }} />
+        </Head>
         <body>
           <Main />
           <NextScript />
